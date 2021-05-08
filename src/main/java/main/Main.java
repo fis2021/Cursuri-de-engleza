f366df78e281dfdaa3c65df4de22dcfc3035d802
Ioana <ioana.capra@outlook.com>
2021-05-08 19:32:43 +0300

    CDE-26 Modified size of the registration scene

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/javafx/registration.fxml"));
         primaryStage.setTitle("Log in");
-        primaryStage.setScene(new Scene(root));
+        primaryStage.setScene(new Scene(root,600,400));
         primaryStage.show();
     }
 
