87753b2be00808d3cd1eb5b09f5e0b707513e507
Ioana <ioana.capra@outlook.com>
2021-05-15 10:37:53 +0300

    put the back button on action in the TeacherLessonsController

---
+++ src/main/java/controllers/TeacherLessonsController.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static javafx.fxml.FXMLLoader.load;
+
+public class TeacherLessonsController implements Initializable {
+    //Back Button -> TeacherHomePage.fxml
+    public void backButtonTeacher(ActionEvent event) throws IOException {
+        Parent parent1 = load(getClass().getResource("/javafx/TeacherHomePage.fxml"));
+
+        Scene parent3_scene = new Scene(parent1);
+        Stage parent3_stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        parent3_stage.setScene(parent3_scene);
+        parent3_stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+}
